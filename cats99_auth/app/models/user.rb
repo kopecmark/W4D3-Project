2a896d8af91276f2b305237fb4519beb5ce4f6ca
kopecmark <mark.kopec@gmail.com>
2018-11-14 14:54:47 -0800

    Finish control model

--- cats99_auth/app/models/user.rb
+++ cats99_auth/app/models/user.rb
@@ -2,10 +2,36 @@ class User < ApplicationRecord
   
   validates :session_token, :user_name, uniqueness: true, presence: true 
   validates :password_digest, presence: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+  before_validation :ensure_session_token
   
-  def self.reset_session_token!
-    self.session_token = SecureRandom.base64
+  attr_reader :password
+  
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
     self.save!
     self.session_token
+  end
+  
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+  
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+  end
+  
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+  
+  def self.find_by_credentials(user_name, password)
+    user = User.find_by(user_name: user_name)
+    if user && user.is_password?(password)
+      user
+    else
+      nil
+    end 
   end
 end
