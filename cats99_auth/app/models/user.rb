c7a8dbea2a1aacc9dcb8b9e79f73dbcca8c79784
kopecmark <mark.kopec@gmail.com>
2018-11-14 14:21:38 -0800

    Write validations for user

--- cats99_auth/app/models/user.rb
+++ cats99_auth/app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  
+  validates :session_token, :user_name, uniqueness: true, presence: true 
+  validates :password_digest, presence: true
+  
+  def self.reset_session_token!
+    self.session_token = SecureRandom.base64
+    self.save!
+    self.session_token
+  end
 end
