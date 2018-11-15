32a071078fadaad6a4ead97403d736a5a2c78d2d
kopecmark <mark.kopec@gmail.com>
2018-11-14 17:26:03 -0800

    Update sessions controller

--- cats99_auth/app/controllers/sessions_controller.rb
+++ cats99_auth/app/controllers/sessions_controller.rb
@@ -18,6 +18,8 @@ class SessionsController < ApplicationController
     if @current_user
       @current_user.reset_session_token!
       session[:session_token] = nil
+      @current_user = nil
+    end
   end
   
   
